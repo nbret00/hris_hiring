b49e1f965f77da8fdd8ad7c777cea22b7977981f
Nino Bretana <nino.bretana@gmail.com>
2017-03-21 07:31:47 +0800

    built

--- target/generated-sources/annotations/com/nino/app/hrishiring/Company_.java
+++ target/generated-sources/annotations/com/nino/app/hrishiring/Company_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-15T17:21:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-21T07:30:27")
 @StaticMetamodel(Company.class)
 public class Company_ { 
 
--- target/generated-sources/annotations/com/nino/app/hrishiring/Sourcing_.java
+++ target/generated-sources/annotations/com/nino/app/hrishiring/Sourcing_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-15T17:21:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-21T07:30:27")
 @StaticMetamodel(Sourcing.class)
 public class Sourcing_ { 
 
