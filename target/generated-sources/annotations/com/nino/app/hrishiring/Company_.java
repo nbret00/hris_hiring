67440ef5005d03225bed2a7ef8ab89d8a3653cf8
Nino Bretana <nino.bretana@gmail.com>
2017-04-04 20:12:18 +0800

    pointing to mocean

--- target/generated-sources/annotations/com/nino/app/hrishiring/Company_.java
+++ target/generated-sources/annotations/com/nino/app/hrishiring/Company_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-04T18:32:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-04T20:11:44")
 @StaticMetamodel(Company.class)
 public class Company_ { 
 
