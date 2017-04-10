324e45f8d3b2fc5260e2e16766376787b4997da1
Nino Bretana <nino.bretana@gmail.com>
2017-04-10 10:01:06 +0800

    POINTING TO MOCEAN

--- target/generated-sources/annotations/com/nino/app/hrishiring/PersonstagestatusPK_.java
+++ target/generated-sources/annotations/com/nino/app/hrishiring/PersonstagestatusPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-10T01:09:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-10T10:00:30")
 @StaticMetamodel(PersonstagestatusPK.class)
 public class PersonstagestatusPK_ { 
 
